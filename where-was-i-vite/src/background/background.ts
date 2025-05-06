8a5f48ba7957da095743ef5c0a119f6ca05d57c8
Yoo Seung-jun <funczun@gmail.com>
2025-05-06 19:53:57 +0900

    feat: optimize sync update logic with early return

--- where-was-i-vite/src/background/background.ts
+++ where-was-i-vite/src/background/background.ts
@@ -4,20 +4,22 @@ chrome.runtime.onMessage.addListener((msg, _sender) => {
 
     chrome.storage.sync.get(["savedSites", "pageData"], (result) => {
       const savedSites = result.savedSites || {};
+
+      if (!savedSites[url]) {
+        return;
+      }
+
       const pageData = result.pageData || {};
 
       const progress = Math.min(scroll / (height - viewport), 1);
       const status = progress >= 0.9 ? "pendingDelete" : "active";
       const lastAccessed = Date.now();
 
-      if (savedSites[url]) {
-        savedSites[url] = {
-          ...savedSites[url],
-          lastAccessed,
-          status,
-        };
-      }
-
+      savedSites[url] = {
+        ...savedSites[url],
+        lastAccessed,
+        status,
+      };
       pageData[url] = { scroll, height, viewport };
 
       chrome.storage.sync.set({ savedSites, pageData });
